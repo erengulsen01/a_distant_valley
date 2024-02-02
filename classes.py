9cb61058870b2fccbcb6f9923154fab202f720bc
erengulsen01 <erengulsen2003@hotmail.com>
2024-02-02 23:57:36 +0300

    added asd

--- classes.py
+++ classes.py
@@ -5,6 +5,8 @@ class Weapons:
         self.cost=cost
         self.name=name
 
+asd = None
+
 class Swords(Weapons):
     def __init__ (self,damage,cooldown,cost,name):
         super().__init__(damage,cooldown,cost,name)
